0e176037da383521d3d0b9b340a5d96ad5617dfe
yuhan9001 <72333033+yuhan9001@users.noreply.github.com>
2020-11-15 01:53:24 +0900

    Student/StudentTest

---
+++ StudentTest.java
@@ -0,0 +1,15 @@
+package calsspart;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student studentLee = new Student();
+		studentLee.studentName="이순신";
+		studentLee.address = "서울"	;
+		
+		studentLee.showStudentInfo();
+
+	}
+
+}
